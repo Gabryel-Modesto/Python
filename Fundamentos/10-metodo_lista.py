f6f4017534ce8a356708bd5208ecdbf80ddc79ee
Gabryel-De-Souza <gabryelpsn@gmail.com>
2025-03-21 16:54:36 -0300

    Estudo sobre Python

---
+++ Fundamentos/10-metodo_lista.py
@@ -0,0 +1,10 @@
+filmsList = ["Motoqueiro Fantasma", 
+             "Missão Impossível", 
+             "Jogos vorazes",
+             "Velozes E Furiosos"]
+
+# 1 - Tamanho da lista
+print(len(filmsList))
+
+# 2 - Recuperar um item da lista pelo índice
+print(filmsList.index("Jogos vorazes"))
---
+++ Fundamentos/9-lista.py
@@ -0,0 +1,21 @@
+# array
+
+# movieMatrix = ["Matrix", 1999, 8.7, True]
+# print(movieMatrix)
+
+filmsList = ["Motoqueiro Fantasma", 
+             "Missão Impossível", 
+             "Jogos vorazes",
+             "Velozes E Furiosos"]
+
+# 1 - Buscar os dois primeiros itens da lista
+print(filmsList[:2])
+
+# 2 - Buscar o último item da lista
+print(filmsList[-1])
+
+# 3 - Buscar filmes até uma determinada posição
+print(filmsList[:3])
+
+# 4 - Buscar filmes de de uma posição em diante
+print(filmsList[1:4])
